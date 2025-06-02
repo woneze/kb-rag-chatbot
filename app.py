122c521262338588da03e487544349f0de075416
woneze <00wonjae@gmail.com>
2025-06-02 15:38:20 +0900

    feat: LangCahin 기반 RAG 체인 구성

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableWithMessageHistory
+from langchain.memory import ChatMessageHistory
+from langchain_openai import ChatOpenAI
 
 
 # 설정
@@ -27,3 +32,39 @@ def initialize_vectorstore():
     chunks = process_pdf()
     embeddings = OpenAIEmbeddings(api_key=api_key)
     return Chroma.from_documents(chunks, embeddings)
+
+# 체인 초기화
+@st.cache_resource
+def initialize_chain():
+    vectorstore = initialize_vectorstore()
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
+
+    template = """당신은 KB 부동산 보고서 전문가입니다. 다음 정보를 바탕으로 사용자의 질문에 답변해주세요.
+    컨텍스트: {context}
+    """
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", template),
+        ("placeholder", "{chat_history}"),
+        ("human", "{question}")
+    ])
+
+    model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, api_key=api_key)
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    base_chain = (
+        RunnablePassthrough.assign(
+            context=lambda x: format_docs(retriever.invoke(x["question"]))
+        )
+        | prompt
+        | model
+        | StrOutputParser()
+    )
+
+    return RunnableWithMessageHistory(
+        base_chain,
+        lambda session_id: ChatMessageHistory(),
+        input_messages_key="question",
+        history_messages_key="chat_history",
+    )
